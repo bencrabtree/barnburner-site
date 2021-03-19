c4edd938a6111162dc17a2d9293fcedc70686c05
bencrab <bencrab@umich.edu>
2021-03-19 10:12:57 -0400

    up and running

---
+++ src/client/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div className='app'>
+                <h1>Barnburner Solutions, LLC</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,10 +1,34 @@
 import express, { Router } from 'express';
+import cors from 'cors';
 const path = require("path");
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const multer = require('multer');
-const upload = multer({ storage: multer.memoryStorage() });
 
 export default (app, settings) => {
     const router = Router();
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        // res.header('Content-Type', 'text/plain')
+        next();
+    });
+    app.use(cookieParser());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    // app.get('*', isAuth);
+    app.use(cors({ origin: true, credentials: true }));
+    app.use(express.static(path.join(__dirname, "../public")));
+
+    ////////////////////////////
+    app.use('/', router);
+    app.get('*/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'),
+        error => {
+            if (error) {
+                console.log('Error Sending html');
+                res.status(500).send(error)
+            }
+        })
+    })
 }
